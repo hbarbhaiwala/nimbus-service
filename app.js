23dc01f8a92fff84e59fd334e095ff0116f5c8e3
Husain Barbhaiwala <hbarbhaiwala@expedia.com>
2018-09-19 22:34:53 -0700

    testing out the order of the routes

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const https = require('https');
-//const request = require('request');
 const baseController = require('./controller/base.controller.js');
 const nimbusController = require('./controller/nimbus-event.controller.js');
 const subAppController = require('./controller/sub-app-controller.js');
@@ -10,16 +8,21 @@ const subAppController = require('./controller/sub-app-controller.js');
  * Set up our server and static page hosting
  */
 const app = express();
-app.use(express.static('public'));
+
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('public'));
 app.use(bodyParser.json());
+
+app.all('*', baseController.log);
+
+app.use('/subapp', subAppController.routes);
 
 app.get('/', baseController.root);
 app.get('/foo', baseController.foo);
-app.use('/subapp', subAppController.routes)
 
 
-app.all('*', baseController.log);
+
 
 
 app.post('/createNimbusEvent', nimbusController.create);
