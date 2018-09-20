61582ba0f4cb0fe9ca3a1a0fe32b26b039f72aa8
Husain Barbhaiwala <hbarbhaiwala@expedia.com>
2018-09-19 23:09:31 -0700

    cleaning up all the routes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const baseController = require('./controller/base.controller.js');
-const nimbusController = require('./controller/nimbus-event.controller.js');
-const subAppController = require('./controller/sub-app-controller.js');
+
+const defaultRoutes = require('./routes/defaultRoutes')
 
 /**
  * Set up our server and static page hosting
@@ -14,21 +13,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.all('*', baseController.log);
-
-app.use('/subapp', subAppController.routes);
-
-app.get('/', baseController.root);
-app.get('/foo', baseController.foo);
-
-
-
-
-
-app.post('/createNimbusEvent', nimbusController.create);
-app.get('/retrieveAllEvents', baseController.middleware, nimbusController.findAll);
-app.get('/retrieveEvent', nimbusController.find);
-app.get('/nimbus-event/:id', nimbusController.findWithId);
+app.use("/", defaultRoutes);
 
 /**
 * Start serving the app.
---
+++ routes/defaultRoutes.js
@@ -0,0 +1,26 @@
+
+var express = require('express')
+var router = express.Router();
+
+// load all the controllers
+const baseController = require('../controller/base.controller.js');
+const nimbusController = require('../controller/nimbus-event.controller.js');
+const subAppController = require('../controller/sub-app-controller.js');
+
+// For all routes do some logging
+router.all('*', baseController.log);
+
+// loading the sub application above applications routes
+router.use('/subapp', subAppController.routes);
+
+// Base controller routes
+router.get('/', baseController.root);
+router.get('/foo', baseController.foo);
+
+// Nimbus-event controller routes
+router.post('/createNimbusEvent', nimbusController.create);
+router.get('/retrieveAllEvents', baseController.middleware, nimbusController.findAll);
+router.get('/retrieveEvent', nimbusController.find);
+router.get('/nimbus-event/:id', nimbusController.findWithId);
+
+module.exports = router;
