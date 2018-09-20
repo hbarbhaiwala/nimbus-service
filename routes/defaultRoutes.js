2df327ec12d25576b8c6518511305f3fadec8e70
Husain Barbhaiwala <hbarbhaiwala@expedia.com>
2018-09-19 23:58:05 -0700

    different type of middle ware

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -7,6 +7,12 @@ const baseController = require('../controller/base.controller.js');
 const nimbusController = require('../controller/nimbus-event.controller.js');
 const subAppController = require('../controller/sub-app-controller.js');
 
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+})
+
 // For all routes do some logging
 router.all('*', baseController.log);
 
