eb59b5b522a0be893bd9c780de4d6bbb0ee08629
Husain Barbhaiwala <hbarbhaiwala@expedia.com>
2018-09-25 02:32:35 -0700

    courses grphql controller

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const baseController = require('../controller/base.controller.js');
 const nimbusController = require('../controller/nimbus-event.controller.js');
 const subAppController = require('../controller/sub-app-controller.js');
 const graphqlController = require('../controller/graphql');
+const coursesGraphqlController = require('../controller/graphql.courses.controller');
 
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
@@ -30,13 +31,18 @@ router.get('/retrieveAllEvents', baseController.middleware, nimbusController.fin
 router.get('/retrieveEvent', nimbusController.find);
 router.get('/nimbus-event/:id', nimbusController.findWithId);
 
-// GraphQL
 
+// GraphQL
 var express_graphql = require('express-graphql');
 
 router.use('/graphql', express_graphql({
     schema: graphqlController.schema,
     rootValue: graphqlController.root,
+    graphiql: true
+}));
+router.use('/courses', express_graphql({
+    schema: coursesGraphqlController.schema,
+    rootValue: coursesGraphqlController.root,
     graphiql: true
 }));
 
