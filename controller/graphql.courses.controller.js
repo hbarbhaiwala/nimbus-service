ebc70b4140686d22163b62fb20a692710feb03f2
Husain Barbhaiwala <hbarbhaiwala@expedia.com>
2018-10-07 23:23:37 -0700

    making author an array of string

--- controller/graphql.courses.controller.js
+++ controller/graphql.courses.controller.js
@@ -18,7 +18,7 @@ exports.schema = buildSchema(`
     type Course {
         id: Int
         title: String
-        author: String
+        author: [String]
         description: String
         topic: String
         url: String
@@ -29,7 +29,7 @@ var coursesData = [
     {
         id: 1,
         title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
+        author: ['Andrew Mead', 'Rob Percival'],
         description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
         topic: 'Node.js',
         url: 'https://codingthesmartway.com/courses/nodejs/'
@@ -37,7 +37,7 @@ var coursesData = [
     {
         id: 2,
         title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
+        author: ['Brad Traversy'],
         description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
         topic: 'Node.js',
         url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
@@ -45,7 +45,7 @@ var coursesData = [
     {
         id: 3,
         title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
+        author: ['Anthony Alicea'],
         description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
         topic: 'JavaScript',
         url: 'https://codingthesmartway.com/courses/understand-javascript/'
